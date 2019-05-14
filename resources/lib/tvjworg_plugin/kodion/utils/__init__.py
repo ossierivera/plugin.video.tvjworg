9d13205e126c6b57e7730583c679f8223e5d1463
Ossie Rivera <os.rivera@gmail.com>
2019-05-14 17:55:45 -0400

    Removing service script

--- resources/lib/tvjworg_plugin/kodion/utils/__init__.py
+++ resources/lib/tvjworg_plugin/kodion/utils/__init__.py
@@ -17,8 +17,8 @@ from .watch_later_list import WatchLaterList
 from .function_cache import FunctionCache
 #from .access_manager import AccessManager
 from .http_server import get_http_server, is_httpd_live, get_client_ip_address
-from .monitor import YouTubeMonitor
-from .player import YouTubePlayer
+#from .monitor import YouTubeMonitor
+#from .player import YouTubePlayer
 from .playback_history import PlaybackHistory
 from .data_cache import DataCache
 from .system_version import SystemVersion
@@ -27,6 +27,5 @@ from . import ip_api
 
 __all__ = ['SearchHistory', 'FavoriteList', 'WatchLaterList', 'FunctionCache',
            'strip_html_from_text', 'create_path', 'create_uri_path', 'find_best_fit', 'to_unicode', 'to_utf8',
-           'datetime_parser', 'select_stream', 'get_http_server', 'is_httpd_live', 'YouTubeMonitor',
-           'make_dirs', 'loose_version', 'ip_api', 'PlaybackHistory', 'DataCache', 'get_client_ip_address',
-           'SystemVersion', 'find_video_id', 'YouTubePlayer']
+           'datetime_parser', 'select_stream', 'get_http_server', 'is_httpd_live', 'make_dirs', 'loose_version', 'ip_api', 'PlaybackHistory', 'DataCache', 'get_client_ip_address',
+           'SystemVersion', 'find_video_id']
